446a4496709f22b4e95117d66e1100c45bfd044a
RaymondW9 <60833199+RaymondW9@users.noreply.github.com>
2020-03-07 23:58:29 -0500

    index, css, konami js, and background images

---
+++ konami.js
@@ -0,0 +1,22 @@
+// konami code (up up down down left right left right b a)
+var konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+// variable to track user's progress
+var position = 0;
+document.addEventListener('keydown', function(e) {
+  var key = e.keyCode;
+  var correctKey = konamiCode[position];
+  if (key == correctKey) {
+    position++;
+    if (position == konamiCode.length) {
+      success();
+      position = 0;
+    }
+  } else {
+    position = 0;
+  }
+});
+
+function success() {
+  alert('Congratulations');
+  window.open('https://www.linkedin.com/in/raymond-wang1/');
+}
